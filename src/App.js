7d4aab8afbcf1e7a66643d0c7fd96f72243acd8b
FrancoRosa <km115.franco@gmail.com>
2021-01-31 22:57:07 -0500

    Real database setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import database from './firebase';
+import { useState } from 'react';
 
-function App() {
+const uploadData = () => {
+  database.ref('data').set({
+    voltage: 190,
+    current: 1.1,
+    date: Date.call(),
+  })
+}
+
+const App = () => {
+  const [date, setDate] = useState('')
+  const savedData = database.ref('data');
+
+  savedData.on('child_changed', snapshot => {
+    const record = snapshot.val();
+    setDate(record);
+  })
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>{date}</p>
+        <button onClick={uploadData}>
+          UploadData
+        </button>
       </header>
     </div>
   );
