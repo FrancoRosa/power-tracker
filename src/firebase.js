26890c4cc04c200426afd292ce021b8b34ba6a16
FrancoRosa <km115.franco@gmail.com>
2021-01-31 21:36:58 -0500

    Add firebase config

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDkA_K0fOD3dZXL4oUsuPtb3JnfK7Ia1q4",
+  authDomain: "pwr-trkr.firebaseapp.com",
+  projectId: "pwr-trkr",
+  storageBucket: "pwr-trkr.appspot.com",
+  messagingSenderId: "686900693096",
+  appId: "1:686900693096:web:2112e0283e2eee0f7c53dd",
+  measurementId: "G-6M5T22JFC1"
+};
