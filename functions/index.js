505be6a80d6abc8e1ad1f4f4aed16998614a3ad9
FrancoRosa <km115.franco@gmail.com>
2021-02-02 14:45:10 -0500

    Add function on request

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,9 @@
-// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
-// The Firebase Admin SDK to access Firestore.
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-// Take the text parameter passed to this HTTP endpoint and insert it into 
-// Firestore under the path /messages/:documentId/original
-exports.addMessage = functions.https.onRequest(async (req, res) => {
-  // Grab the text parameter.
-  console.log('>>>>>>>>>>>>>>>>')
-  console.log(req.body.text)
-  console.log(req.body)
-  const original = req.body.text;
-  // Push the new message into Firestore using the Firebase Admin SDK.
-  const writeResult = await admin.firestore().collection('messages').add({text: original});
-  // Send back a message that we've successfully written the message
+exports.addText = functions.https.onRequest(async (req, res) => {
+  const text = req.body.text;
+  const writeResult = await admin.firestore().collection('texts').add({text: text});
   res.json({result: `Message with ID: ${writeResult.id} added.`});
 });
--- functions/servertest.py
+++ functions/servertest.py
@@ -1,5 +1,6 @@
 import requests
-url = 'http://localhost:5001/pwr-trkr/us-central1/addMessage'
+url = 'http://localhost:5001/pwr-trkr/us-central1/addText'
+url = 'https://us-central1-pwr-trkr.cloudfunctions.net/addText'
 message = {'text': 'cool'}
 
 response = requests.post(url, json=message)
